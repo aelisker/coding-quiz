bee3ee8d16f0fa6dfd51789edc04ffad000c83e4
Aaron Lisker <aelisker@gmail.com>
2020-04-04 19:42:37 -0700

    found sort function at https://www.educative.io/edpresso/how-to-sort-an-array-of-objects-in-javascript to sort objects in an array

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -9,6 +9,29 @@ var clearScores = function () {
   loadScores();
 }
 
+ //sort function taken from https://www.educative.io/edpresso/how-to-sort-an-array-of-objects-in-javascript
+var sortScores = function () {
+  function dynamicsort(property,order) {
+    var sort_order = 1;
+    if(order === "desc"){
+        sort_order = -1;
+    }
+    return function (a, b){
+        // a should come before b in the sorted order
+        if(a[property] < b[property]){
+                return -1 * sort_order;
+        // a should come after b in the sorted order
+        }else if(a[property] > b[property]){
+                return 1 * sort_order;
+        // a and b are the same
+        }else{
+                return 0 * sort_order;
+        }
+    }
+  }
+  savedScores.sort(dynamicsort("score", "desc"));
+};
+
 var loadScores = function () {
     //load scored only if array is not a null value, otherwise function breaks
     var isArrayNull = localStorage.getItem("scores");
@@ -21,6 +44,8 @@ var loadScores = function () {
       scoreAreaEl.style.fontWeight = '600';
       scoreAreaEl.textContent = 'There are currently no saved scores.';
     }
+
+    sortScores();
 
     var highScoreListEl = document.createElement("ul");
     highScoreListEl.className = 'score-ul';
